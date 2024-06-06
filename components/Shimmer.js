0bca368729f7f12daf8d41f44d3fc5c0275cb3da
Raj Dhiman <rajdhiman469@gmail.com>
2024-06-06 12:55:58 +0530

    Shimmer UI Added

--- components/Shimmer.js
+++ components/Shimmer.js
@@ -3,17 +3,34 @@ import load from "../files/shimmer_img.gif"
 const Shimmer = ()=>{
     return(
         <div>
-            
+            <h2> </h2>
             <div className="shimmer_ui">
-            
+                <div className="shimmer_card">
                 <img src={load} className="shimmer_img"/>
+                </div>
+                <div className="shimmer_card">
                 <img src={load} className="shimmer_img"/>
+                </div>
+                <div className="shimmer_card">
                 <img src={load} className="shimmer_img"/>
+                </div>
+                <div className="shimmer_card">
                 <img src={load} className="shimmer_img"/>
+                </div>
+                <div className="shimmer_card">
                 <img src={load} className="shimmer_img"/>
+                </div>
+                <div className="shimmer_card">
                 <img src={load} className="shimmer_img"/>
+                </div>
+                <div className="shimmer_card">
                 <img src={load} className="shimmer_img"/>
+                </div>
+                <div className="shimmer_card">
                 <img src={load} className="shimmer_img"/>
+                </div>
+                
+                
             
             </div>
         </div>
