f91cdfa7635be7ee268e39732f9a7ce7f6365ad9
Raj Dhiman <rajdhiman469@gmail.com>
2024-06-06 15:53:13 +0530

    bug-fixes

--- data/genres.js
+++ data/genres.js
@@ -2,7 +2,7 @@ const genres = [
     "Art", "Biography", "Business", "Children's", "Christian", "Classics", 
     "Comedy", "Comics", "Contemporary", "Cookbooks", "Crime", "Fantasy", "Fiction",
     "Historical Fiction", "History", "Horror", "Humor", "Memoir", "Music", "Mystery", 
-    "Nonfiction", "Paranormal", "Philosophy", "Poetry", "Psychology", "Religion", "Romance", 
+    "Non-Fiction", "Paranormal", "Philosophy", "Poetry", "Psychology", "Religion", "Romance", 
     "Science", "Science-Fiction", "Self-Help", "Suspense", "Spirituality", "Sports", 
     "Thriller", "Travel", "Young Adult"]
 
