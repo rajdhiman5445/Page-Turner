355ebfe833f2dace6f740ce4e7b4e1b72bb97b28
Raj Dhiman <rajdhiman469@gmail.com>
2024-07-06 11:13:52 +0530

    changed-server-url

--- data/url.js
+++ data/url.js
@@ -1,5 +1,6 @@
 //const server_url = "http://localhost:3000/getInfo"
 
-const server_url = "https://pageturnerserver.onrender.com/getInfo"
+//const server_url = "https://pageturnerserver.onrender.com/getInfo"
+const server_url = "https://page-turner-server.vercel.app/getInfo";
 
 export default server_url;
