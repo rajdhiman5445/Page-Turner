14bb49f35d5b7cb628859e7e8c2bc2dd7c82ad13
Raj Dhiman <rajdhiman469@gmail.com>
2024-08-13 22:07:31 +0530

    added new category

--- components/Body.js
+++ components/Body.js
@@ -11,11 +11,13 @@ const Body = () => {
             <>
             <div className="app_body">
                 <Block list="Best Sellers You Can't Miss"/>
+                <FilteredBlock list="Dive into the World of Discovery"/>
                 <FilteredBlock list="Must-Read Thriller Hits"/>
                 <FilteredBlock list="Gas Station Horrors: Read If You Dare"/>
                 <FilteredBlock list="Top Non-Fiction Books"/>
                 <FilteredBlock list="Most Popular Fiction Books"/>
                 <FilteredBlock list="Spine-Tingling Horror Hits"/>
+                
             </div>
             </>
         )
