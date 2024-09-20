213c9ce3b6daef209a1d3167ecbf9b70db23665f
Raj Dhiman <rajdhiman469@gmail.com>
2024-09-20 14:44:49 +0530

    fixed navigation in the mobile view

--- data/types.js
+++ data/types.js
@@ -2,12 +2,12 @@ import search from "../files/types/search.svg"
 import highlights from "../files/types/highlights.svg"
 import genres from "../files/types/genres.svg"
 import quote from "../files/types/quotes.svg"
-import books from "../files/types/books.svg"
+import library from "../files/types/library.svg"
 
 const types = [
     {id: 1, link: "/search", img: search, title:"Search for books"},
     {id:2, link: "/categories", img: genres, title:"Explore all Genres"},
-    {id:3, link: "/books", img: books, title:"Explore Featured Books"},
+    {id:3, link: "/library", img: library, title:"Explore Library"},
     {id:4, link:"/home", img: highlights, title:"Get Highlights"},
     {id:5, link:"/quotes", img: quote, title:"Explore Quotes"}
 ]
